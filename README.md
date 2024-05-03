d2e030115073c6177bdbf2d406d551d561f9dfc1
Chris Gillatt <github@basegreen.net>
2024-05-03 14:35:01 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# [![CI](https://github.com/chris-gillatt/tmaj/actions/workflows/main.yml/badge.svg)](https://github.com/chris-gillatt/tmaj/actions/workflows/main.yml) Tell me a Joke (Tmaj)
+A command-line 'app' which produces a random joke, with a couple of amusing options.  Used to demonstrate brew packaging and distribution. 
+
+# Usage
+```
+Usage: tmaj [OPTIONS]
+
+Options:
+    -h             Display this help message
+    -c             Print a joke in coloured font
+    -f             Print a joke in large text
+    -j             Print a joke with no frills
+    -s             Print a joke inside a speech bubble 
+    -v             Print tmaj current version
+```
+
+# Installation
+Install/uninstall/upgrade tmaj using brew:
+
+`brew tap chris-gillatt/tmaj` 
+
+`brew install tmaj`
+
---
+++ package.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -e
+# Brew package script
+export APP="tmaj"
+REPO_NAME="tmaj"
+
+git config user.name "$USER_NAME"
+git config user.email "$USER_EMAIL"
+
+export RELEASE_COUNT="$GITHUB_RUN_NUMBER"
+GIT_REVISION=$(git rev-parse HEAD)
+
+BRANCH="master"
+
+# Compress new version and place in tars directory
+mkdir tars
+tar cvf tars/"${APP}-0.1.${RELEASE_COUNT}.tar.gz" "$APP"
+
+# Generate Ruby file for Brew using template
+erb "${APP}.erb" > "${APP}.rb"
+
+## Git Tasks
+git add "${APP}.rb"
+# Commit and push files to repo
+git commit -m "Push $APP Release 0.1.${RELEASE_COUNT}" &&
+git push https://"${USER_NAME}:${USER_PASSWORD}@github.com/${ORG}/${REPO_NAME}".git "$BRANCH"
+
+# Publish go cli binaries
+post_release_json()
+{
+  cat <<EOF
+{
+  "tag_name":         "0.1.${RELEASE_COUNT}",
+  "target_commitish": "${GIT_REVISION}",
+  "name":             "0.1.${RELEASE_COUNT}"
+}
+EOF
+}
+
+echo "Creating release.."
+
+NEW_RELEASE_RESPONSE=$(curl --silent \
+                            --write-out "\n%{http_code}" \
+                            -u "$USER_NAME:$USER_PASSWORD" \
+                            -H "Accept: application/json" \
+                            -H "Content-Type:application/json" \
+                            -X POST "https://api.github.com/repos/${ORG}/${REPO_NAME}/releases" \
+                            --data "$(post_release_json)")
+STATUS_CODE=$(echo "$NEW_RELEASE_RESPONSE" | tail -n 1)
+NEW_RELEASE=$(echo "$NEW_RELEASE_RESPONSE" | sed '$d')
+
+if [[ $STATUS_CODE -ge 400 ]]; then
+  echo 'ERROR: Failed to create release'
+  echo "$STATUS_CODE"
+  echo "$NEW_RELEASE"
+  exit 1
+fi
+
+echo "Release created"
+
+UPLOAD_URL=$(echo "$NEW_RELEASE" | jq -r .upload_url | cut -f1 -d"{")
+
+echo "Uploading binaries"
+
+curl --fail \
+     -u "${USER_NAME}:${USER_PASSWORD}" \
+     -H "Content-Type:application/octet-stream" \
+     -X POST "${UPLOAD_URL}?name=${APP}-0.1.${RELEASE_COUNT}.tar.gz" \
+     --data-binary "@tars/${APP}-0.1.${RELEASE_COUNT}.tar.gz" \
+     | jq -rc '.name + " - " + .url + " - " + .state'
+
+echo "$0 Done."
+
