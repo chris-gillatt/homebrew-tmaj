e69a841817bf52983131f37408c8a29281b2b61d
Chris Gillatt <github@basegreen.net>
2024-05-10 10:11:49 +0800

    revert to old version of package.sh

--- package.sh
+++ package.sh
@@ -1,25 +1,10 @@
 #!/bin/bash -e
 # Brew package script
-
-announce () {
-  echo "$(basename "$0"): $@"
-} # End announce
-
-announce "-----ENV------"
-env 
-announce "-----END ENV------"
-
-git config user.email "$USER_EMAIL"
-git config user.name "$USER_NAME"
-
-announce "-----GIT CONFIG------"
-git config --list
-announce "-----END GIT CONFIG------"
-
-announce "$USER_NAME"
-
 export APP="tmaj"
 REPO_NAME="tmaj"
+
+git config --global user.name "$USER_NAME"
+git config --global user.email "$USER_EMAIL"
 
 export RELEASE_COUNT="$GITHUB_RUN_NUMBER"
 GIT_REVISION=$(git rev-parse HEAD)
@@ -36,12 +21,8 @@ erb "${APP}.erb" > "${APP}.rb"
 ## Git Tasks
 git add "${APP}.rb"
 # Commit and push files to repo
-git commit -m "Push $APP Release 0.0.${RELEASE_COUNT}"
-announce "-----GIT STATUS------"
-git status
-announce "-----END GIT status------"
-announce "Path to push: https://chris-gillatt:${USER_PASSWORD}@github.com/${ORG}/${REPO_NAME}.git $BRANCH"
-git push "https://chris-gillatt:${USER_PASSWORD}@github.com/${ORG}/${REPO_NAME}.git $BRANCH"
+git commit -m "Push $APP Release 0.0.${RELEASE_COUNT}" &&
+git push https://"${GH_USER_NAME}:${USER_PASSWORD}@github.com/${ORG}/${REPO_NAME}".git "$BRANCH"
 
 # Publish go cli binaries
 post_release_json()
@@ -55,11 +36,11 @@ post_release_json()
 EOF
 }
 
-announce "Creating release.."
+echo "Creating release.."
 
 NEW_RELEASE_RESPONSE=$(curl --silent \
                             --write-out "\n%{http_code}" \
-                            -u "$GITHUB_ACTOR:$USER_PASSWORD" \
+                            -u "$GH_USER_NAME:$USER_PASSWORD" \
                             -H "Accept: application/json" \
                             -H "Content-Type:application/json" \
                             -X POST "https://api.github.com/repos/${ORG}/${REPO_NAME}/releases" \
@@ -68,24 +49,23 @@ STATUS_CODE=$(echo "$NEW_RELEASE_RESPONSE" | tail -n 1)
 NEW_RELEASE=$(echo "$NEW_RELEASE_RESPONSE" | sed '$d')
 
 if [[ $STATUS_CODE -ge 400 ]]; then
-  announce 'ERROR: Failed to create release'
-  announce "$STATUS_CODE"
-  announce "$NEW_RELEASE"
+  echo 'ERROR: Failed to create release'
+  echo "$STATUS_CODE"
+  echo "$NEW_RELEASE"
   exit 1
 fi
 
-announce "Release created"
+echo "Release created"
 
 UPLOAD_URL=$(echo "$NEW_RELEASE" | jq -r .upload_url | cut -f1 -d"{")
 
-announce "Uploading binaries"
+echo "Uploading binaries"
 
 curl --fail \
-     -u "${USER_NAME}:${USER_PASSWORD}" \
+     -u "${GH_USER_NAME}:${USER_PASSWORD}" \
      -H "Content-Type:application/octet-stream" \
      -X POST "${UPLOAD_URL}?name=${APP}-0.0.${RELEASE_COUNT}.tar.gz" \
      --data-binary "@tars/${APP}-0.0.${RELEASE_COUNT}.tar.gz" \
      | jq -rc '.name + " - " + .url + " - " + .state'
 
-announce "Done."
-announce "Release created successfully."
+echo "$0 Done."
